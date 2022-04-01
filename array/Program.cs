377219979b7a3e7112b947827a7d800d9cfaad07
Михаил Денисов <migelius.invesy@gmail.com>
2022-04-01 22:26:41 +0300

    Добавьте файлы проекта.

---
+++ array/Program.cs
@@ -0,0 +1,40 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int[,] two_Dimensional_array = { { 5, 2, 3, 7 }, { 4, 4, 6, 9 }, { 7, 8, 9, 1 } };
+        int arrayStringCounter;
+        int arrayColumnCounter;
+        int sumOfSecondLine = 0;
+        int firstColumnMultiplication = 1;
+        int numberString = 1;
+        int numberColumn = 0;
+
+        for (arrayStringCounter = 0; arrayStringCounter < two_Dimensional_array.GetLength(0); arrayStringCounter++)
+        {
+            for (arrayColumnCounter = 0; arrayColumnCounter < two_Dimensional_array.GetLength(1); arrayColumnCounter++)
+            {
+                Console.Write(two_Dimensional_array[arrayStringCounter, arrayColumnCounter]);
+            }
+            Console.WriteLine();
+        }
+
+        for (arrayStringCounter = 0; arrayStringCounter < two_Dimensional_array.GetLength(0); arrayStringCounter++)
+        {
+            for (arrayColumnCounter = 0; arrayColumnCounter < two_Dimensional_array.GetLength(1); arrayColumnCounter++)
+            {
+                if (arrayStringCounter == numberString)
+                {
+                    sumOfSecondLine += two_Dimensional_array[arrayStringCounter, arrayColumnCounter];
+                }
+
+                if (arrayColumnCounter == numberColumn)
+                {
+                    firstColumnMultiplication = two_Dimensional_array[arrayStringCounter, arrayColumnCounter] * firstColumnMultiplication;
+                }
+            }
+        }
+        Console.WriteLine($"Сумма второй строки массива - {sumOfSecondLine}\nПроизведение первого столбца массива - {firstColumnMultiplication}");
+    }
+
+}
