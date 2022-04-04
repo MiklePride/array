8c769a13269b351b9e9e6ccb92675a4ababcb9b0
Михаил Денисов <migelius.invesy@gmail.com>
2022-04-04 18:10:24 +0300

    re

--- array/Program.cs
+++ array/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        int[,] two_Dimensional_array = { { 5, 2, 3, 7 }, { 4, 4, 6, 9 }, { 7, 8, 9, 1 } };
+        int[,] two_dimensional_array = { { 5, 2, 3, 7 }, { 4, 4, 6, 9 }, { 7, 8, 9, 1 } };
         int arrayStringCounter;
         int arrayColumnCounter;
         int sumOfSecondLine = 0;
@@ -10,22 +10,24 @@
         int numberString = 1;
         int numberColumn = 0;
 
-        for (arrayStringCounter = 0; arrayStringCounter < two_Dimensional_array.GetLength(0); arrayStringCounter++)
+        for (arrayStringCounter = 0; arrayStringCounter < two_dimensional_array.GetLength(0); arrayStringCounter++)
         {
-            for (arrayColumnCounter = 0; arrayColumnCounter < two_Dimensional_array.GetLength(1); arrayColumnCounter++)
+            for (arrayColumnCounter = 0; arrayColumnCounter < two_dimensional_array.GetLength(1); arrayColumnCounter++)
             {
-                Console.Write(two_Dimensional_array[arrayStringCounter, arrayColumnCounter]);
+                Console.Write(two_dimensional_array[arrayStringCounter, arrayColumnCounter]);
             }
             Console.WriteLine();
         }
-        for (arrayColumnCounter = 0; arrayColumnCounter < two_Dimensional_array.GetLength(0); arrayColumnCounter++)
+
+        for (arrayColumnCounter = 0; arrayColumnCounter < two_dimensional_array.GetLength(0); arrayColumnCounter++)
         {
-            firstColumnMultiplication *= two_Dimensional_array[arrayColumnCounter, numberColumn];
+            firstColumnMultiplication *= two_dimensional_array[arrayColumnCounter, numberColumn];
         }
-        for (arrayStringCounter = 0; arrayStringCounter < two_Dimensional_array.GetLength(1); arrayStringCounter++)
+
+        for (arrayStringCounter = 0; arrayStringCounter < two_dimensional_array.GetLength(1); arrayStringCounter++)
         {
-            sumOfSecondLine += two_Dimensional_array[numberString, arrayStringCounter];
+            sumOfSecondLine += two_dimensional_array[numberString, arrayStringCounter];
         }
         Console.WriteLine($"Произведение первой колонки - {firstColumnMultiplication}\nСумма второй строки - {sumOfSecondLine}");
-    } 
+    }
 }
